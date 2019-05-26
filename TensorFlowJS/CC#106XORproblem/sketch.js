4695c60a5954c0cd4fa052c271922402072a2434
Creativeguru97 <worldapple2630@gmail.com>
2019-05-26 20:24:16 +0900

    May 26, 2019

--- TensorFlowJS/CC#106XORproblem/sketch.js
+++ TensorFlowJS/CC#106XORproblem/sketch.js
@@ -1,57 +1,113 @@
-let nn;
-let lr_slider;
-let training_data = [{
-    inputs:[1, 0],
-    targets:[1]
-  },
-  {
-    inputs:[0, 1],
-    targets:[1]
-  },
-  {
-    inputs:[1, 1],
-    targets:[0]
-  },
-  {
-    inputs:[0, 0],
-    targets:[0]
-  }
-];
-//Interesting syntax !!!!!!!!!!!!!!!!
+// let nn;
+let NNmodel;
+// let lr_slider;
+
+let resolution = 25;
+let pixRows;
+let pixCols;
+let tensorInputs;
+
+const trainInputs = tf.tensor2d([
+  [1, 1],
+  [1, 0],
+  [0, 1],
+  [0, 0]
+]);
+const trainOutputs = tf.tensor2d([
+  [0],
+  [1],
+  [1],
+  [0]
+]);
 
 function setup(){
   createCanvas(400, 400);
-  nn = new NeuralNetwork(2, 12, 1);//We can set arbitrary number of hidden node
+  pixRows = height / resolution;
+  pixCols = width / resolution;
 
-  //Creates a slider <input></input> element in the DOM
-  //(min val, max val, default val, step size of slider);
-  lr_slider = createSlider(0.001, 0.5, 0.1, 0.01);
-  }
-
-function draw(){
-  background(0);
-
-  nn.learningRate(lr_slider.value());
-  print(nn.learning_rate);
-
-  for(let i = 0; i < 5000; i++){
-    let data = random(training_data);
-      nn.train(data.inputs, data.targets);
-  }
-
-  let resolution = 10;
-  let pixRows = height / resolution;
-  let pixCols = width / resolution;
+  //Create the input data
+  let inputs = [];
   for(let i = 0; i < pixCols; i++){
     for(let j = 0; j < pixRows; j++){
       let x0 = i / pixCols;//Get a value from 0 to 1
       let x1 = j / pixRows;
-      let inputs = [x0, x1];
-      let y = nn.feedforward(inputs);
-      fill(y*255);
-      noStroke();
-      rect(i*resolution, j*resolution, resolution, resolution);
+      inputs.push([x0, x1]);
     }
   }
+
+  tensorInputs = tf.tensor2d(inputs);
+
+  NNmodel = tf.sequential();
+  const hidden = tf.layers.dense({
+    units:6,//Number of nodes
+    inputShape: [2],
+    // activation: "relu"
+    activation: "sigmoid"
+  });
+  NNmodel.add(hidden);
+
+  const output = tf.layers.dense({
+    units: 1,//Number of nodes
+    inputShape: [6],
+    // activation: "relu"
+    activation: "sigmoid"
+  });
+  NNmodel.add(output);
+
+  const sgdOpt = tf.train.adam(0.2);//adam loss function is great!!!
+  // const sgdOpt = tf.train.sgd(0.2);
+
+  //Compile the configured model
+  NNmodel.compile({
+    optimizer: sgdOpt,
+    loss: "meanSquaredError"
+  });
+
+  setTimeout(train, 10);//100milisecond wait then call train();
+}
+
+function train(){
+  trainModel().then((result) => {//tf.fit automatically clean up tensors
+    console.log(result.history.loss[0]);
+
+      setTimeout(train, 10);//100milisecond wait then call train();
+  });
+}
+function trainModel(){
+      const config = {
+        shuffle: true,
+        epochs : 10
+      }
+      return NNmodel.fit(trainInputs, trainOutputs, config);
+      // console.log(response.history.loss[0]);
+    // }
+  }
+
+
+function draw(){
+  background(0);
+  //Get the prediction
+  /*We create tensor, and give that to GPU.
+  GPU calculate then give them back to us with dataSync();*/
+  tf.tidy(() =>{
+    let testOutputs = NNmodel.predict(tensorInputs).dataSync();
+    // console.log(testOutputs);
+
+    //Draw the results
+    let index = 0;
+    for(let i = 0; i < pixCols; i++){
+      for(let j = 0; j < pixRows; j++){
+
+        fill(testOutputs[index]*255);
+        noStroke();
+        rect(i*resolution, j*resolution, resolution, resolution);
+        fill(255-testOutputs[index]*255);
+        textSize(8);
+        textAlign(CENTER, CENTER);
+        text(nf(testOutputs[index],1, 2), i*resolution+resolution/2, j*resolution+resolution/2);
+        index++;
+      }
+    }
+  });
 
 }
