3cc520878d0685af6f3fa63383174735e37e9a93
Creativeguru97 <worldapple2630@gmail.com>
2019-12-14 16:18:08 +0900

    Dec 14,  2019

--- O'Reilly'_DeepLearning/Chapter04_Learning/derivative.py
+++ O'Reilly'_DeepLearning/Chapter04_Learning/derivative.py
@@ -1,14 +1,45 @@
 import numpy as np
+import matplotlib.pylab as plt
 
 #A bad exsample
+# def numerical_diff0(f, x):
+#     h = 10e-50
+#     return (f(x+h) - f(x)) / h
 #1: 10e-50 is too tiny to handle in computer. It treated as 0.0 by rounding error.
 #2: This formura can't find actual inclination of the f(x) at x.
 #But between x and x+h.
-def numerical_diff(f, x):
-    h = 10e-50
-    return (f(x+h) - f(x)) / h
 
 #Fixed version!
-def numerical_diff2(f, x):
+def numerical_diff1(f, x):
     h = 1e-4 #0.0001
     return (f(x+h) - f(x-h)) / (2*h)
+
+
+# Example function to check our numerical differentiation
+def function_1(x):
+    return 0.01 * x**2 + 0.1*x
+
+def tangent_line(f, x):
+    d = numerical_diff1(f, x)
+    print(d)
+    y = f(x) - d * x
+    return lambda t: d*t + y
+
+x = np.arange(0.0, 20.0, 0.1) #Generate value 0 to 20 every 0.1
+y = function_1(x)
+plt.xlabel("x")
+plt.ylabel("f(x)")
+
+# tf = tangent_line(function_1, 5)
+tf = tangent_line(function_1, 10)
+
+y2 = tf(x)
+
+
+
+# a0 = numerical_diff1(function_1, 5) #Actually: 0.2
+# a1 = numerical_diff1(function_1, 10)#Actually: 0.3
+
+plt.plot(x, y)
+plt.plot(x, y2)
+plt.show()
---
+++ O'Reilly'_DeepLearning/Chapter04_Learning/partial_derivative.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pylab as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def numerical_diff1(f, x):
+    h = 1e-4 #0.0001
+    return (f(x+h) - f(x-h)) / (2*h)
+
+
+
+def function_2(x):#We expect an numpy array as input
+    return x[0]**2 + x[1]**2
+    #Or np.sum(x**2)
+
+
+def tangent_line(f, x):
+    d = numerical_diff1(f, x)
+    print(d)
+    y = f(x) - d * x
+    return lambda t: d*t + y
+
+x0 = np.arange(-2, 2.5, 0.25)
+x1 = np.arange(-2, 2.5, 0.25)
+X, Y = np.meshgrid(x0, x1)
+
+X = X.flatten()
+Y = Y.flatten()
+
+
+y = function_1(x)
+plt.xlabel("x")
+plt.ylabel("f(x)")
+
+
+
+# a0 = numerical_diff1(function_1, 5) #Actually: 0.2
+# a1 = numerical_diff1(function_1, 10)#Actually: 0.3
+
+plt.plot(x, y)
+plt.plot(x, y2)
+plt.show()
