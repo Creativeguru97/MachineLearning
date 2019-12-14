eb1f076feef9842c490b52c6f3b1f7272cc1618d
Creativeguru97 <worldapple2630@gmail.com>
2019-12-14 18:18:34 +0900

    Dec 14, 2019

--- O'Reilly'_DeepLearning/Chapter04_Learning/partial_derivative.py
+++ O'Reilly'_DeepLearning/Chapter04_Learning/partial_derivative.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pylab as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def numerical_diff1(f, x):
+def numerical_diff(f, x):
     h = 1e-4 #0.0001
     return (f(x+h) - f(x-h)) / (2*h)
 
@@ -12,30 +12,41 @@ def function_2(x):#We expect an numpy array as input
     return x[0]**2 + x[1]**2
     #Or np.sum(x**2)
 
+def function_tmp1(x0):
+    #derivative against x0, and held x1 as a constant
+    return x0*x0 + 4.0 ** 2.0
 
-def tangent_line(f, x):
-    d = numerical_diff1(f, x)
-    print(d)
-    y = f(x) - d * x
-    return lambda t: d*t + y
+def function_tmp2(x1):
+    #derivative against x0, and held x1 as a constant
+    return x0*x0 + 4.0 * 2.0
 
-x0 = np.arange(-2, 2.5, 0.25)
-x1 = np.arange(-2, 2.5, 0.25)
-X, Y = np.meshgrid(x0, x1)
-
-X = X.flatten()
-Y = Y.flatten()
-
-
-y = function_1(x)
-plt.xlabel("x")
-plt.ylabel("f(x)")
+print(numerical_diff(function_tmp1, 3.0))
 
 
 
-# a0 = numerical_diff1(function_1, 5) #Actually: 0.2
-# a1 = numerical_diff1(function_1, 10)#Actually: 0.3
-
-plt.plot(x, y)
-plt.plot(x, y2)
-plt.show()
+# def tangent_line(f, x):
+#     d = numerical_diff1(f, x)
+#     print(d)
+#     y = f(x) - d * x
+#     return lambda t: d*t + y
+#
+# x0 = np.arange(-2, 2.5, 0.25)
+# x1 = np.arange(-2, 2.5, 0.25)
+# X, Y = np.meshgrid(x0, x1)
+#
+# X = X.flatten()
+# Y = Y.flatten()
+#
+#
+# y = function_1(x)
+# plt.xlabel("x")
+# plt.ylabel("f(x)")
+#
+#
+#
+# # a0 = numerical_diff1(function_1, 5) #Actually: 0.2
+# # a1 = numerical_diff1(function_1, 10)#Actually: 0.3
+#
+# plt.plot(x, y)
+# plt.plot(x, y2)
+# plt.show()
