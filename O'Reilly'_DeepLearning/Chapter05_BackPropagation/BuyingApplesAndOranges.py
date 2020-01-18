d7c4f207a1940f20f61274d7d989ef159c5949d0
Creativeguru97 <worldapple2630@gmail.com>
2020-01-18 14:02:27 +0900

    Jan 17, 2020

--- O'Reilly'_DeepLearning/Chapter05_BackPropagation/BuyingApplesAndOranges.py
+++ O'Reilly'_DeepLearning/Chapter05_BackPropagation/BuyingApplesAndOranges.py
@@ -29,27 +29,41 @@ class AddLayer:
         dy = dout * 1
         return dx, dy
 
+
 an_apple_price = 100
 apple_num = 2
+an_orange_price = 150
+orange_num = 3
 tax_rate = 1.1
 
 #Layer
 mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
 mul_tax_layer = MulLayer()
 
 #forward
 apples_price = mul_apple_layer.forward(an_apple_price, apple_num) # => this.x = 100, this.y = 2
-total_price = mul_tax_layer.forward(apples_price, tax_rate)# => this.x = 200, this.y = 1.1
+oranges_price = mul_orange_layer.forward(an_orange_price, orange_num) # => this.x = 150, this.y = 3
+fruits_price = add_apple_orange_layer.forward(apples_price, oranges_price) #x = 200, y = 450
+total_price = mul_tax_layer.forward(fruits_price, tax_rate)# => this.x = 650, this.y = 1.1
 
 print(int(total_price))
 
 #backward
 dPrice = 1
-dApples_price, dTax_rate = mul_tax_layer.backward(dPrice)#1
-#dApples_price: dx = dPrice * this.y = 1 * 1.1 = 1.1
-#dTax_rate: dy = dPrice * this.x = 1 * 200 = 200
+dFruits_price, dTax_rate = mul_tax_layer.backward(dPrice)#1
+# dFruits_price: dx = dPrice * this.y = 1 * 1.1 = 1.1
+# dTax_rate: dy = dPrice * this.x = 1 * 650 = 650
+dApples_price, dOranges_price = add_apple_orange_layer.backward(dFruits_price)#1.1
+# dApples_price: dx = dFruits_price * 1 = 1.1 * 1 = 1.1
+# dOranges_price: dy = dFruits_price * 1 = 1.1 * 1 = 1.1
+dAn_orange_price, dOrange_num = mul_orange_layer.backward(dOranges_price)#1.1
+# dAn_orange_price: dx = dOranges_price * this.y = 1.1 * 3 = 3.3
+# dOrange_num: dy = dOranges_price * this.x = 1.1 * 150 = 165
 dAn_apple_price, dApple_num = mul_apple_layer.backward(dApples_price)#1.1
 # dAn_apple_price: dx = dApples_price * this.y = 1.1 * 2 = 2.2
 # dApple_num: dy = dApples_price * this.x = 1.1 * 100 = 110
 
-print(dApples_price, dApple_num, dTax_rate)#(2.2, 110, 200)
+
+print(dAn_apple_price, dApple_num, dAn_orange_price, dOrange_num, dTax_rate)#(2.2, 110, 3.3, 165, 650)
